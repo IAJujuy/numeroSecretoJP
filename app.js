338922af37153801188e7dd9ab3aa98c87e683d4
IAJujuy <egestionserra@gmail.com>
2025-08-22 12:58:52 -0300

    numeroSecretoJP

--- app.js
+++ app.js
@@ -1,42 +1,90 @@
-let numeroSecreto = generarNumeroSecreto();
-let intentos = 1;
 
-console.log(numeroSecreto);
+// Declaro variables globales
+let numeroMaximo = 100;
+let numeroSecreto;
+let intentos = 0;
+let listaNumerosSorteados = [];
 
+// Función genérica para asignar texto a un elemento HTML
 function asignarTextoElemento(elemento, texto) {
     let elementoHTML = document.querySelector(elemento);
     elementoHTML.innerHTML = texto;
-return;
-}
-    
-function mensajesIniciales() {
-    asignarTextoElemento('h1', 'Juego del Número Secreto');
-    asignarTextoElemento('p', 'Indica un número entre 1 y 100'); 
-}
-    
-
-function verificarIntento() {
-    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
-    
-   if (numeroDeUsuario === numeroSecreto) {
-        asignarTextoElemento('p', `¡Felicidades! Acertaste el número en ${intentos} ${intentos === 1 ? 'intento' : 'intentos'}`);
-    } else {
-        if (numeroDeUsuario > numeroSecreto) {
-            asignarTextoElemento('p', 'El numero es menor');
-        } else {
-            asignarTextoElemento('p', 'El numero es mayor');
-        } 
-    } 
-    if (intentos >= 5) {
-        asignarTextoElemento('p', `Lo siento, has agotado tus ${intentos} intentos. El número era ${numeroSecreto}.`);
-    }
-    intentos++;
     return;
 }
 
+// Evento para el botón de verificar intento
+function verificarIntento() {
+    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
+    
+    // El usuario acertó
+    if (numeroDeUsuario === numeroSecreto) {
+        asignarTextoElemento('p',`Acertaste el número en ${intentos} ${(intentos === 1) ? 'vez' : 'veces'}`);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } else {
+        //El usuario no acertó.
+        if (numeroDeUsuario > numeroSecreto) {
+            asignarTextoElemento('p','El número secreto es menor');
+        } else {
+            asignarTextoElemento('p','El número secreto es mayor');
+        }
+    if (intentos >= 5) {
+        asignarTextoElemento('p', `Lo siento, has agotado tus ${intentos} intentos. El número era ${numeroSecreto}.`);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+        limpiarCaja();
+        return;
+    }
+        intentos++;
+        limpiarCaja();
+    }
+    return;
+}
+
+function limpiarCaja() {
+    document.querySelector('#valorUsuario').value = '';
+}
+
 function generarNumeroSecreto() {
-    return Math.floor(Math.random() * 100) + 1;
+    let numeroGenerado = Math.floor(Math.random()*numeroMaximo)+1;
+
+    console.log(numeroGenerado);
+    console.log(listaNumerosSorteados);
+    // Si ya sorteamos todos los numeros posibles, reiniciar el juego
+    if (listaNumerosSorteados.length == numeroMaximo) {
+        asignarTextoElemento('p', 'Todos los números han sido sorteados');
+        document.querySelector('#verificar').setAttribute('disabled','true');
+        return;
+    }
+
+    // si el numero generado esta incluido en la lista de numeros sorteados
+    if (listaNumerosSorteados.includes(numeroGenerado)) {
+        //generar otro numero
+        return generarNumeroSecreto();
+    } else {
+        //si no esta incluido, agregarlo a la lista y retornarlo
+        listaNumerosSorteados.push(numeroGenerado);
+        return numeroGenerado;
+    }
+}
+
+function condicionesIniciales() {
+    asignarTextoElemento('h1','Juego del número secreto!');
+    asignarTextoElemento('p',`Indica un número del 1 al ${numeroMaximo} e intenta adivinar el numero secreto.`);
+    numeroSecreto = generarNumeroSecreto();
+    intentos = 1;
+    console.log(numeroSecreto);
 }
 
 
-mensajesIniciales();
+function reiniciarJuego() {
+    //limpiar caja
+    limpiarCaja();
+    //Indicar mensaje de intervalo de números 
+    //Generar el número aleatorio
+    //Inicializar el número intentos
+    condicionesIniciales();
+    //Deshabilitar el botón de nuevo juego
+    document.querySelector('#reiniciar').setAttribute('disabled','true');
+    
+}
+
+condicionesIniciales();
